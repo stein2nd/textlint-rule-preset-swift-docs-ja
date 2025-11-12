30964182b9b907f4ac8740aa2e690cdb34e350d6
Koutarou ISHIKAWA <stein2nd@gmail.com>
2025-11-12 17:42:12 +0900

    Update index.ts

--- src/preset/index.ts
+++ src/preset/index.ts
@@ -1,19 +1,18 @@
 export default {
-    rules: {
-      "preset-ja-technical-writing": true,
-      terminology: {
-        defaultTerms: false,
-        terms: new URL("../../dict/terminology.json", import.meta.url).pathname
-      },
-      "no-doubled-joshi": true,
-      "ja-no-abusage": true,
-      "ja-space-between-half-and-full-width": true
+  rules: {
+    "preset-ja-technical-writing": true,
+    terminology: {
+      defaultTerms: false,
+      terms: new URL("../../dict/terminology.json", import.meta.url).pathname
     },
-    filters: {
-      comments: true,
-      code: {
-        allow: ["swift"]
-      }
+    "no-doubled-joshi": true,
+    "ja-no-abusage": true,
+    "ja-space-between-half-and-full-width": true
+  },
+  filters: {
+    comments: true,
+    code: {
+      allow: ["swift"]
     }
-  };
-  
+  }
+};
